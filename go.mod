03f7f8d2517f2e74776efee411dcf56dace2ff14
kg0r0 <kg0r0@yahoo.co.jp>
2024-01-10 01:46:03 +0900

    Change processing related to VC

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/kg0r0/go-vc-example
 
 go 1.21.4
+
+require github.com/piprate/json-gold v0.5.0
+
+require github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
--- main.go
+++ main.go
@@ -4,43 +4,81 @@ import (
 	"crypto/ed25519"
 	"fmt"
 	"time"
+
+	"github.com/piprate/json-gold/ld"
 )
+
+type KeyPair struct {
+	publicKey  ed25519.PublicKey
+	privateKey ed25519.PrivateKey
+}
+
+func (k KeyPair) createCredential(claim map[string]interface{}) (interface{}, error) {
+	proc := ld.NewJsonLdProcessor()
+	options := ld.NewJsonLdOptions("")
+	expanded, err := proc.Expand(claim, options)
+	if err != nil {
+		return nil, err
+	}
+	for _, v := range expanded {
+		proof := map[string]interface{}{
+			"type":               "DataIntegrityProof",
+			"cryptosuite":        "ed25519",
+			"created":            time.Now(),
+			"verificationMethod": "https://university.example/issuers/14#key-1",
+			"proofPurpose":       "assertionMethod",
+			"proofValue":         ed25519.Sign(k.privateKey, []byte(fmt.Sprintf("%v", v))),
+		}
+		v.(map[string]interface{})["proof"] = proof
+	}
+	return expanded[0], nil
+}
+
+func (k KeyPair) verifyCredential(credential interface{}) bool {
+	proof := credential.(map[string]interface{})["proof"].(map[string]interface{})
+	delete(credential.(map[string]interface{}), "proof")
+	return ed25519.Verify(k.publicKey, []byte(fmt.Sprintf("%v", credential)), proof["proofValue"].([]byte))
+}
+
+func (k KeyPair) createPresentation(claim map[string]interface{}, credential interface{}) (interface{}, error) {
+	return nil, nil
+}
+
+func (k KeyPair) verifyPresentation(presentation interface{}) bool {
+	return false
+}
 
 func main() {
 	issuerPubKey, issuerPrivKey, _ := ed25519.GenerateKey(nil)
-	keyPair := KeyPair{
+	k := KeyPair{
 		publicKey:  issuerPubKey,
 		privateKey: issuerPrivKey,
 	}
 
-	metadata := IssuerMetadata{
-		context:   []string{"https://www.w3.org/ns/credentials/v2", "https://www.w3.org/ns/credentials/examples/v2"},
-		id:        "http://university.example/credentials/1872",
-		types:     []string{"VerifiableCredential", "ExampleAlumniCredential"},
-		issuer:    "https://university.example/issuers/565049",
-		validFrom: time.Now(),
-	}
-
-	claim := Claim{
-		id: "did:example:ebfeb1f712ebc6f1c276e12ec21",
-		degree: Degree{
-			name:  "Bachelor of Science and Arts",
-			types: "ExampleBachelorDegree",
+	claim := map[string]interface{}{
+		"@context": []interface{}{
+			"https://www.w3.org/ns/credentials/v2",
+			"https://www.w3.org/ns/credentials/examples/v2",
+		},
+		"id": "http://university.example/credentials/1872",
+		"type": []interface{}{
+			"VerifiableCredential",
+			"ExampleAlumniCredential",
+		},
+		"issuer":    "https://university.example/issuers/565049",
+		"validFrom": "2010-01-01T19:23:24Z",
+		"credentialSubject": map[string]interface{}{
+			"id": "did:example:ebfeb1f712ebc6f1c276e12ec21",
+			"alumniOf": map[string]interface{}{
+				"id":   "did:example:c276e12ec21ebfeb1f712ebc6f1",
+				"name": "Example University",
+			},
 		},
 	}
-	vc := createCredential(keyPair, metadata, claim)
-	fmt.Println(verifyCredential(keyPair.publicKey, vc))
-
-	holderPubKey, holderPrivKey, _ := ed25519.GenerateKey(nil)
-	holderKeyPair := KeyPair{
-		publicKey:  holderPubKey,
-		privateKey: holderPrivKey,
+	vc, err := k.createCredential(claim)
+	if err != nil {
+		panic(err)
 	}
-
-	holderMetadata := HolderMetadata{
-		context: []string{"https://www.w3.org/ns/credentials/v2", "https://www.w3.org/ns/credentials/examples/v2"},
-		types:   []string{"VerifiablePresentation", "ExamplePresentation"},
-	}
-	vp := createPresentation(holderKeyPair, holderMetadata, vc)
-	fmt.Println(verifyPresentation(holderKeyPair.publicKey, vp))
+	ld.PrintDocument("Verifiable Credential: ", vc)
+	fmt.Printf("Result: %v\n", k.verifyCredential(vc))
 }
